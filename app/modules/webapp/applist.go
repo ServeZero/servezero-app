dec1488b13f91c61d116b8851949ec1a2e3a677d
naoki hirata <naoki@magic3.org>
2022-02-16 14:12:42 +0900

    アプリケーションアイコン追加。

--- app/modules/webapp/applist.go
+++ app/modules/webapp/applist.go
@@ -13,7 +13,7 @@ var AppInfoList []AppInfo
 
 func init() {
 	AppInfoList = []AppInfo{
-		{"wordpress", "WordPress", "", "", "https://ja.wordpress.org/latest-ja.tar.gz", "5.9"},
-		{"joomla", "Joomla!", "", "", "https://downloads.joomla.org/cms/joomla4/4-1-0/Joomla_4-1-0-Stable-Full_Package.tar.gz", "4.1.0"},
+		{"wordpress", "WordPress", "wordpress.png", "", "https://ja.wordpress.org/latest-ja.tar.gz", "5.9"},
+		{"joomla", "Joomla!", "joomla.png", "", "https://downloads.joomla.org/cms/joomla4/4-1-0/Joomla_4-1-0-Stable-Full_Package.tar.gz", "4.1.0"},
 	}
 }
